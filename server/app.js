ed53965921794ede6d9d8e9dfe9836c163150361
Beesto <ramarajendran9585@gmail.com>
2026-06-14 00:17:26 +0530

    add signup API

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const User = require("./models/user");
+const ejsMate = require("ejs-mate");
+
+const bcrypt = require("bcrypt");
+
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.engine("ejs",ejsMate);
+app.set("view engine","ejs") ;
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log("DB connected");
@@ -8,6 +21,57 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch(err=>{
         console.log("Db not connected")
     })
+
+
+
+
+app.post("/auth/api/signup",async (req,res)=>{
+    try{
+        const {email , firstname , lastname, password} = req.body ;
+        const user = await User.findOne({email});
+        if(user){
+            return res.send({
+                message:"User already exists",
+                sucess : false
+            })
+        }
+    
+        const hashedPass = bcrypt.hashSync(password, 10);
+        const newUser = new User({email,firstname,lastname,password:hashedPass});
+        await newUser.save();
+        res.send({
+                message:"User added",
+                sucess : true
+            });
+    }catch(err){
+        res.send({
+            message:err.message,
+            success:false
+        })
+    }
+    
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/form",(req,res)=>{
+    res.render("test.ejs");
+})
+
 
 
 module.exports = app ;
---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+userSchema =new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true
+    },
+    profilePic:{
+        type:String,
+       
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+},{timestamps:true});
+
+const User = mongoose.model("users",userSchema);
+
+module.exports = User;
+
